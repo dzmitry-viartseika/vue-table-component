c6acfc961334c5765455b0303f90bb8a93a81631
Dmitry Verteyko <verteyko1990@gmail.com>
2020-03-11 22:47:17 +0200

    unit tests

--- tests/unit/table.spec.js
+++ tests/unit/table.spec.js
@@ -1,10 +1,50 @@
-import { shallowMount } from '@vue/test-utils';
-import vTable from '../../src/components/v-table.vue';
 import Home from '../../src/views/Home.vue';
+import getters from '../../src/store/index.js';
 
 describe('Home', () => {
-  test('отрисовывает компонент Child', () => {
-    const wrapper = shallowMount(Home);
-    expect(wrapper.find(vTable).exists()).toBe(true);
+  it('get userlist', () => {
+    let url = '';
+    let body = {};
+
+    jest.mock('axios', () => ({
+      post: (_url, _body) => new Promise((resolve) => {
+        url = _url;
+        body = _body;
+        resolve(true);
+      }),
+    }));
+  });
+});
+
+const users = [
+  {
+    id: '5c41a982ecd00502420c8eb9',
+    name: 'Patti Robles',
+    points_earned: 3005,
+    points_spent: 4562,
+    registration_date: '2017-05-05',
+  },
+  {
+    id: '5c41a982c2060fa6059e928a',
+    name: 'Janna Dickerson',
+    points_earned: 2793,
+    points_spent: 2956,
+    registration_date: '2019-01-15',
+  },
+  {
+    id: '5c41a9827edfe714bf673f6b',
+    name: 'Annmarie Phelps',
+    points_earned: 725,
+    points_spent: 792,
+    registration_date: '2014-05-02',
+  },
+];
+const state = { users };
+
+describe('users', () => {
+  it('return usersList', () => {
+    const actual = getters.users(state);
+
+    expect(actual).toEqual([users[0], users[2]]);
   });
 });
